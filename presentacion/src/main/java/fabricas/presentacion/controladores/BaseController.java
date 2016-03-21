2ccb1155a9a09a674b95613016dc79941f483ac7
Nicolas Jimenez <estnicjimsae@hotmail.com>
2016-03-21 10:06:29 -0500

    Cambios

--- presentacion/src/main/java/fabricas/presentacion/controladores/BaseController.java
+++ presentacion/src/main/java/fabricas/presentacion/controladores/BaseController.java
@@ -12,7 +12,8 @@ public class BaseController {
 	private static final String VIEW_INDEX = "indexAlojamiento";
 	private static final String CONTACTENOS = "contactenos";
 	private static final String REGISTRO = "registro";
-	private static final String CARRITO = "carrito";	
+	private static final String CARRITO = "carrito";
+	private static final String TRANSPORTE = "indexTransporte";
 	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
@@ -49,6 +50,17 @@ public class BaseController {
 	@RequestMapping(value = "/carrito", method = RequestMethod.GET)
 	public String verCarrito(ModelMap model) {
 		return CARRITO;
+
+	}
+	
+	/**
+	 * Controlador de la pantalla busqueda transporte
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/indexTransporte", method = RequestMethod.GET)
+	public String transporte(ModelMap model) {
+		return TRANSPORTE;
 
 	}
 }
